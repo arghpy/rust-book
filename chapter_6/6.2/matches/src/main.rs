6f0b019e60a2fbf4627ceb241bd278753e6fdb9e
arghpy <subaandrei2@gmail.com>
2024-06-27 07:18:16 +0300

    chapter 6.2

--- chapter_6/6.2/matches/src/main.rs
+++ chapter_6/6.2/matches/src/main.rs
@@ -6,11 +6,28 @@ enum Coin {
 }
 
 fn main() {
-    let my_coin = Coin::Dime;
-    println!("My coin is {} cents.", value_in_cents(my_coin));
+    println!("My coin is {:.2} dollars.", (value_in_cents(&Coin::Penny) as f32) / 100_f32);
+    println!("My coin is {:.2} dollars.", (value_in_cents(&Coin::Nickel) as f32) / 100_f32);
+    println!("My coin is {:.2} dollars.", (value_in_cents(&Coin::Dime) as f32) / 100_f32);
+    println!("My coin is {:.2} dollars.", (value_in_cents(&Coin::Quarter) as f32) / 100_f32);
+
+    let mut my_coin = value_in_cents(&Coin::Nickel);
+    println!(
+        "Current value: {}\nAfter addition: {:?}",
+        &my_coin, add_one_dollar(&Some(my_coin))
+    );
+
+    match my_coin {
+        5 => {
+            println!("Lucky coin!");
+            my_coin += 10;
+            println!("You receive 10 more cents. New value: {}", my_coin)
+        }
+        _ => (),
+    };
 }
 
-fn value_in_cents(coin: Coin) -> u8 {
+fn value_in_cents(coin: &Coin) -> i32 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
@@ -19,5 +36,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             10
         },
         Coin::Quarter => 25,
+    }
+}
+
+fn add_one_dollar(coin: &Option<i32>) -> Option<i32> {
+    match coin {
+        None => None,
+        Some(value) => Some(value + 1)
     }
 }
